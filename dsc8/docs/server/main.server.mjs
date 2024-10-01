d322f7705fdad8ef1bc6d8a3246cc4c230e9155e
Fabio O.S. Netto <99618673+fabioosnetto@users.noreply.github.com>
2024-10-01 11:41:16 -0300

    deploy application to docs folder

---
+++ dsc8/docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-JHWL2QHC.mjs";import"./chunk-3S2C5KFS.mjs";import"./chunk-PC7VYF5F.mjs";import"./chunk-2L36GP5T.mjs";import"./chunk-PC6AFUGU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
